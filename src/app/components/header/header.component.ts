a3bccf5d986a288a03bed5fef3ac75cab8f8a84a
Lucas <caslumach@gmail.com>
2023-10-05 18:37:31 -0300

    ajustes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  up = 'pi-angle-up'
 
   optionsMenu = [
     { option: 'Sobre', id: 1 },
     { option: 'Projetos', id: 2 },
     { option: 'Contato', id: 3 }
   ];
+  
+  ngOnInit(): void {
+    
+  }
 
   navbarVisible: boolean = false;
 
   toggleNavbar() {
     debugger;
     this.navbarVisible = !this.navbarVisible;
+    if (!this.navbarVisible) {
+      this.up = 'pi-angle-down'
+    } 
   }
 
   handleImageError() {
