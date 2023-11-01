062ce784a37c1bea8ad17d2c8c94834349868acf
Lucas <caslumach@gmail.com>
2023-10-31 22:16:07 -0300

    teste mapa

--- src/app/pages/modulos/contact/contact.component.ts
+++ src/app/pages/modulos/contact/contact.component.ts
@@ -31,16 +31,16 @@ export class ContactComponent implements OnInit {
       this.map = new Map('map').setView([-23.511224828565467, -46.87626884189107], 13);
     }
 
-    // tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   maxZoom: 20,
-    //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    // }).addTo(this.map);
-
-    tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-      minZoom: 0,
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 20,
-      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
+
+    // tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    //   minZoom: 0,
+    //   maxZoom: 20,
+    //   attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(this.map);
 
     marker([-23.511224828565467, -46.87626884189107]).addTo(this.map)
       .bindPopup('Eu estou aqui')
