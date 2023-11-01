bcd6bf76d4788e253072415582f3e9acf8934ead
Lucas <caslumach@gmail.com>
2023-10-31 23:13:52 -0300

    ajustes

--- src/app/pages/modulos/contact/contact.component.ts
+++ src/app/pages/modulos/contact/contact.component.ts
@@ -49,7 +49,9 @@ export class ContactComponent implements OnInit {
     //   maxZoom: 20
     // }).addTo(this.map); //funciona 
 
-    tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+    
+
+    tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token=2j0fNjzrZ6xMbZmjIW7FhMLDCixa8A65hquyivkjVRIcviMF8DgRSKkrNYKBxzgu', {
       attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
       maxZoom: 22,
