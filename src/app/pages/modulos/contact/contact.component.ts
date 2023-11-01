107ab66841370ae3cc8f2707119e559687059bc3
Lucas <caslumach@gmail.com>
2023-10-31 23:00:10 -0300

    ajustes mapa

--- src/app/pages/modulos/contact/contact.component.ts
+++ src/app/pages/modulos/contact/contact.component.ts
@@ -19,7 +19,7 @@ export class ContactComponent implements OnInit {
     { nome: 'Telefone', link: '', icon: 'pi pi-phone', },
 
   ]
-  ehMobile: any 
+  ehMobile: any
   ngOnInit(): void {
     this.ehMobile = window.screen.width < 992;
     console.log(this.locale);
@@ -34,7 +34,7 @@ export class ContactComponent implements OnInit {
     // tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     //   maxZoom: 20,
     //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    // }).addTo(this.map);
+    // }).addTo(this.map); // funciona 
 
     // tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
     //   minZoom: 0,
@@ -43,13 +43,21 @@ export class ContactComponent implements OnInit {
     // }).addTo(this.map);
 
 
-   tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20
-}).addTo(this.map);
+    // tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    //   subdomains: 'abcd',
+    //   maxZoom: 20
+    // }).addTo(this.map); //funciona 
 
-    
+    tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+      attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 0,
+      maxZoom: 22,
+      subdomains: 'abcd',
+      accessToken: 'PyTJUlEU1OPJwCJlW1k0NC8JIt2CALpyuj7uc066O7XbdZCjWEL3WYJIk6dnXtps'
+    }).addTo(this.map); //funciona 
+
+
 
     marker([-23.511224828565467, -46.87626884189107]).addTo(this.map)
       .bindPopup('Eu estou aqui')
