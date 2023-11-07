f2532ef5ce84123ea43bfd21da9774370e9b099b
Lucas <caslumach@gmail.com>
2023-11-07 18:23:24 -0300

    deploy

--- src/app/components/modais/modal-viewer/modal-viewer.component.ts
+++ src/app/components/modais/modal-viewer/modal-viewer.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TraductionService } from 'src/app/core/services/translate.service';
 
 @Component({
   selector: 'app-modal-viewer',
   templateUrl: './modal-viewer.component.html',
 })
-export class ModalViewerComponent {
+export class ModalViewerComponent implements OnInit {
 
   @Input() selectItem?: any;
   constructor(
-
+    private traductionService: TraductionService,
+    private translate: TranslateService,
   ) {
 
   }
@@ -23,6 +26,21 @@ export class ModalViewerComponent {
   ngOnInit(): void {
     this.ehMobile = window.screen.width < 992;
 
+    this.languageUser()
+  }
+
+  getLabel(e: any): string {
+    return this.translate.instant(e);
+  }
+
+  userLanguage: any
+  languageUser = () => {
+    this.userLanguage = this.traductionService.getUserLanguage();
+    this.translate.setDefaultLang(this.userLanguage);
+  }
+
+  getTranslationKey() {
+    return `modalViewer.${this.selectItem?.nome.toLowerCase()}.desc`;
   }
 
   openModal = () => (this.displayModal = true);
